87b2d4af46c0d3804fc31b89ae2a2a9dc0f04360
lautarosantillan <santillanlautaro03@gmail.com>
2022-06-24 02:47:30 -0300

    Route -v3 (Posible SOLUCION)

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,88 +1,102 @@
-//IMPORTS
-import { useEffect, useState } from "react"
-import axios from "axios";
-//Componente
-import ItemDetail from "../ItemDetail/ItemDetail"
-// import { getProductById } from "../../productos"
-//React-Router-DOM
-import { Link } from "react-router-dom";
-import { useParams } from 'react-router-dom';
-
-const ItemDetailContainer = () => {
-    const [item, setItem] = useState([]);
-
-    // const id = 3
-    let { id } = useParams();
-
-    useEffect(() => {
-        axios(`https://62aa950c371180affbd78121.mockapi.io/api/prod-ps5/articulos/${id}`).then((res) =>
-            setItem(res.data)
-        );
-    }, [id]);
-
-    console.log("Item:", item)
-
-    return (
-        <>
-            <div className='ItemDetail-Container'>
-                <h2> DETALLE DEL PRODUCTO </h2>
-                <hr />
-                {/* <ItemDetail {...item} /> */}
-                {item.map((prod) => {
-                    return(
-                        <div key={prod.id}>
-                            <Link to={`/item/${prod.id}`}>
-                                <ItemDetail {...prod} />
-                            </Link>
-                        </div>
-                    )
-                })}
-            </div>
-        </>
-    )
-}
-
-export default ItemDetailContainer;
-
+// //IMPORTS
 // import { useEffect, useState } from "react"
-// import { getProductById } from "../../productos"
+// import axios from "axios";
+// //Componente
 // import ItemDetail from "../ItemDetail/ItemDetail"
-// // import { Link } from "react-router-dom";
-// // import { useParams } from 'react-router-dom';
+// // import { getProductById } from "../../productos"
+// //React-Router-DOM
+// import { Link } from "react-router-dom";
+// import { useParams } from 'react-router-dom';
 
 // const ItemDetailContainer = () => {
 //     const [item, setItem] = useState([]);
-    
-//     const id = "2"
 
-//     // let { id } = useParams();
+//     // const id = 3
+//     let { id } = useParams();
 
 //     useEffect(() => {
-//         getProductById(id)
-//             .then(res => {
-//                 setItem(res)
-//             }
-//             )
-//     }, [])
+//         axios(`https://62aa950c371180affbd78121.mockapi.io/api/prod-ps5/articulos/${id}`).then((res) =>
+//             setItem(res.data)
+//         );
+//     }, [id]);
 
 //     console.log("Item:", item)
 
+        // const styles = {
+        //     div:{
+        //         paddingTop: 130
+        //     }
+        // }
+
 //     return (
-//         <div className='ItemDetail-Container'>
-//             <h2> DETALLE DEL PRODUCTO </h2>
-//             <hr />
-//             <ItemDetail {...item} />
-//             {/* {item.map((item) => {
-//                 return(
-//                     <div key={item.id}>
-//                         <Link to={`/item/${item.id}`}>
-//                             <ItemDetail {...item} />
-//                         </Link>
-//                     </div>
-//                 )
-//             })} */}
-//         </div>
+//         <>
+//             <div className='ItemDetail-Container' style={styles.div}>
+//                 <h2> DETALLE DEL PRODUCTO </h2>
+//                 <hr />
+//                 {/* <ItemDetail {...item} /> */}
+//                 {item.map((prod) => {
+//                     return(
+//                         <div key={prod.id}>
+//                             <Link to={`/item/${prod.id}`}>
+//                                 <ItemDetail {...prod} />
+//                             </Link>
+//                         </div>
+//                     )
+//                 })}
+//             </div>
+//         </>
 //     )
 // }
 
 // export default ItemDetailContainer;
+
+import { useEffect, useState } from "react"
+import { getProductById } from "../../productos"
+import ItemDetail from "../ItemDetail/ItemDetail"
+import { Link } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+
+const ItemDetailContainer = () => {
+    const [product, setProduct] = useState([]);
+
+    let { id } = useParams();
+
+    useEffect(() => {
+        getProductById(id)
+            .then(res => {
+                setProduct(res)
+            }
+            )
+    }, [id])
+
+    console.log("Product:", product)
+
+    const styles = {
+        div:{
+            paddingTop: 130
+        }
+    }
+
+    return (
+        <div className='ItemDetail-Container' style={styles.div}>
+            <h2> DETALLE DEL PRODUCTO </h2>
+            <hr />
+            <div key={product.id}>
+                <Link to={`/item/${product.id}`}>
+                    <ItemDetail {...product} />
+                </Link>
+            </div>
+            {/* {product.map((item) => {
+                return(
+                    <div key={item.id}>
+                        <Link to={`/item/${item.id}`}>
+                            <ItemDetail {...item} />
+                        </Link>
+                    </div>
+                )
+            })} */}
+        </div>
+    )
+}
+
+export default ItemDetailContainer;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -19,7 +19,6 @@ const Home = () => {
     <div className='MainSection' style={styles.MainSection}>
         <Navbar />
         <ItemListContainer />
-        <ItemDetailContainer />
     </div>
     );
 };
