908b8e05ab28b3e5022147aedfc7727f906783c5
lautarosantillan <santillanlautaro03@gmail.com>
2022-07-13 00:29:25 -0300

    Styles Form

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -25,9 +25,41 @@ const Shop = () => {
         <div style={styles.div}>
             <h2 className="title-shop"><span>PARA FINALIZAR SU COMPRA:</span> Ingrese sus datos personales en el formulario</h2>
             <hr></hr>
-            <div className="form-shop">
-                <h3>Formulario - Shop</h3>
-            </div>
+            <article className="d-flex justify-content-center">
+                <div className="card col-sm-6 p-3 formulario">
+                    <div className="mb-3">
+                        <h2>Formulario de Pago</h2>
+                    </div>
+                    <form action="">
+                        <div className="row">
+                            <div className="col">
+                                <label for="nombre" className="form-label">Nombre</label>
+                                <input type="text" className="form-control" placeholder="Ejemplo: Lautaro" aria-label="First name" required/>
+                            </div>
+                            <div className="col">
+                                <label for="apellido" className="form-label">Apellido</label>
+                                <input type="text" className="form-control" placeholder="Ejemplo: Santillan" aria-label="Last name" required/>
+                            </div>
+                        </div>
+                        <div classNameName="mb-2">
+                            <label for="numero" className="form-label">Numero Telefonico</label>
+                            <input type="number" className="form-control" name="numero" placeholder="" required />
+                        </div>
+                        <div classNameName="mb-2">
+                            <label for="correo" className="form-label">Correo electronico</label>
+                            <input type="text" className="form-control" name="correo" placeholder="ejemplo@mail.com" required />
+                        </div>
+                        <div classNameName="mb-2">
+                            <label for="correo" className="form-label">Repita su Correo electronico</label>
+                            <input type="text" className="form-control" name="correo" placeholder="ejemplo@mail.com" required />
+                        </div>
+                        <div className="mb-2 btns-form">
+                            <input type="submit" value="REALIZAR COMPRA" className="btn-form" />
+                            <input type="reset" value="BORRAR" className="btn-form" />
+                        </div>
+                    </form>
+                </div>
+            </article>
             <div className="items-shop">
                 <h4>Items del Carrito:</h4>
                 <div className="item-shop-header">
