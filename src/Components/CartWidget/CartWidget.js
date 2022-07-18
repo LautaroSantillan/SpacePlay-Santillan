0fc0813ee742ed8b0cb1d1251749eeb18f8c422c
lautarosantillan <santillanlautaro03@gmail.com>
2022-07-17 22:58:32 -0300

    Styles

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -8,15 +8,17 @@ import { useContext } from "react";
 import { CartContext } from "../../Context/CartContext";
 //React-Router-DOM
 import { NavLink } from "react-router-dom";
-
+//DEVELOPING
 const CartWidget = () => {
-    const { items } = useContext(CartContext)
-    let itemInCart = 0
+    const { items } = useContext(CartContext);
+    
+    let itemInCart = 0; //Inicializo el contador
 
+    //Mapea los items que hay y los suma
     items.map((item) => {
-        itemInCart = itemInCart + item.qty
-        return itemInCart
-    })
+        itemInCart = itemInCart + item.qty;
+        return itemInCart;
+    });
 
     return (
         <div className="cart">
